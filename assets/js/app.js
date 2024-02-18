3b33b2eefe860b13b477c5ecaf2a020d78eb99e3
Raúl Castro <castroherreraraul@gmail.com>
2024-02-18 18:21:56 -0300

    Terminé

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,4 +2,13 @@ $(enviarCorreo).click(function () {
     alert("Su solicitud fue enviada correctamente")
 });
 
+$(document).ready(function() {
+    $(window).resize(function() {
+        if ($(window).width() < 576) { // Cambia 768 según tu criterio
+            $("¿QUIENES SOMOS?").hide();
+        } else {
+            $("¿QUIENES SOMS").show();
+        }
+    });
+});
 
